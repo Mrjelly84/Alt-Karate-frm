065fb39b405e4e54b05607fc91d1d4560617713d
william dew <williamrdew@gmail.com>
2025-09-11 18:14:20 +0100

    aaa

--- Add Member.vb
+++ Add Member.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Add_Member
+    Dim id As Integer
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            KarateBindingSource.EndEdit()
+            MembersTableAdapter1.Update(karateDBDataSet.Members)
+        Catch ex As Exception
 
+        End Try
+    End Sub
+
+    Private Sub Add_Member_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        KarateBindingSource.AddNew()
+        id = MembersTableAdapter1.GetNewMemberID()
+        IDTextBox.Text = id.ToString()
+    End Sub
 End Class
